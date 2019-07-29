f16f65b8f6caca6f7379df0dd31ee9ddf1164215
anderzheng <zhengqihua@163.com>
2019-07-30 05:08:29 +0800

    Update ex7.c
    pass

--- ex7.c
+++ ex7.c
@@ -1,23 +1,18 @@
 
 
 int reverse(int x){
-    unsigned int absX = 0;
     long sumVal = 0;
     
-    if (x < 0)
-        absX = -x;
-    else
-        absX = x;
-    while(absX > 0)
+    while(x != 0)
     {
-        sumVal = sumVal*10 + (absX%10);
-        absX /= 10;
+        sumVal = sumVal*10 + (x%10);
+        x /= 10;
     }
     
-    if (sumVal > 0x7FFFFFFF || (x <0 && sumVal > (unsigned int)0x80000000))
+    if (sumVal > 2147483647 || (sumVal < -2147483648))
         return 0;
     
-    return x<0?-sumVal:sumVal;
+    return (int)sumVal;
 
 }
 
