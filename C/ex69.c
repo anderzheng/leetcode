fa32667bd217b9535b2c28776977aa93769e6da1
anderzheng <zhengqihua@163.com>
2019-09-21 10:11:21 +0800

    Update ex69.c
    modify comment

--- C/ex69.c
+++ C/ex69.c
@@ -2,7 +2,7 @@
 /* find the digits n, that  (n+1)*(n+1) > x && n*n < x */
 
 int mySqrt(int x){
-    unsigned int i = 0;         /* notice: must use unsigned 
+    unsigned int i = 0;         /* notice: must use unsigned int */
     unsigned int tmpVal = 0;
     
     for(i=0; i<=x; i++)     /* notice: i<=x, when x=2, i need to try x */
