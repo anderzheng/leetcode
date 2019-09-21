193400df57bbd6ce7ed5bc04ce459fe02e5b1b7e
anderzheng <zhengqihua@163.com>
2019-09-21 10:09:04 +0800

    Create ex69.c
    20ms pass

---
+++ C/ex69.c
@@ -0,0 +1,23 @@
+/* nomoral method */
+/* find the digits n, that  (n+1)*(n+1) > x && n*n < x */
+
+int mySqrt(int x){
+    unsigned int i = 0;         /* notice: must use unsigned 
+    unsigned int tmpVal = 0;
+    
+    for(i=0; i<=x; i++)     /* notice: i<=x, when x=2, i need to try x */
+    {
+        tmpVal = i*i;
+        if (tmpVal == x)
+        {
+            break;
+        }
+        else if(tmpVal > x)
+        {
+            i--;
+            break;
+        }
+    }
+    return i;
+}
+
