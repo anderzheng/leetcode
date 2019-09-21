9e621b9d25045744f59b958a98f8faf939f88b1c
anderzheng <zhengqihua@163.com>
2019-09-21 10:49:46 +0800

    Update ex69.c

--- C/ex69.c
+++ C/ex69.c
@@ -1,4 +1,4 @@
-/* nomoral method */
+/* nomoral method: 20ms */
 /* find the digits n, that  (n+1)*(n+1) > x && n*n < x */
 
 int mySqrt(int x){
@@ -19,5 +19,44 @@ int mySqrt(int x){
         }
     }
     return i;
+}
+
+/* binary divide method: 4ms */
+/* find the digits n, that  (n+1)*(n+1) > x && n*n < x */
+
+int mySqrt(int x){
+    int right = 0, mid = 0, left = x;         /* notice: must use unsigned int 64 */
+    unsigned long long tmpVal = 0;
+    
+    while(left - right > 1)
+    {
+        mid = (left+right)/2;
+        tmpVal = (unsigned long long)mid*mid;           /* notice: tmpVal maybe greater than unsigned int
+                                        so, need to use int64 */
+        
+        if (tmpVal < x)
+        {
+            right = mid;
+        }
+        else if (tmpVal > x)
+        {
+            left = mid;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    
+    if ((unsigned long long)left*left == x)
+    {
+        return left;
+    }
+    else
+    {
+        return right;
+    }
+    
+    return right;
 }
 
