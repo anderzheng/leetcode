1e3693d7abd13d5ca8337db1951c05e40e121d5a
anderzheng <zhengqihua@163.com>
2019-09-22 14:42:02 +0800

    Update ex88.c

--- c/ex88.c
+++ c/ex88.c
@@ -1,3 +1,34 @@
+
+/* merget from back, without malloc backup array, passed 4ms */
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    int i = 0, j= 0, k= 0;
+    
+    for(i=m-1,j=n-1,k=m+n-1;i>=0&& j>=0;k--)
+    {
+        if (nums1[i] < nums2[j])
+        {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        else
+        {
+            nums1[k] = nums1[i];
+            i--;
+        }
+    }
+    for (;i>=0; i--)             // copy last data from nums1
+    {
+        nums1[k--] = nums1[i];
+    }
+    for(;j>=0; j--)             // copy last data from nums2
+    {
+        nums1[k--] = nums2[j];
+    }
+    
+    return;
+}
+
+
 /* use a tmp array, passed 8ms */
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
