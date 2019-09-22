fe82a5cf9de45b3744efaf4951684cf667f584db
anderzheng <zhengqihua@163.com>
2019-09-22 21:51:46 +0800

    Update ex101.c

--- C/ex101.c
+++ C/ex101.c
@@ -31,3 +31,62 @@ bool isSymmetric(struct TreeNode* root){
 
 }
 
+//-------------------------------------------------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+/* iteratively method of DFS: passed 12ms */
+
+bool isSymmetric(struct TreeNode* root){
+    
+    int s1 = 0, s2 = 0;
+    struct TreeNode *ptr1 = NULL, *ptr2 = NULL;
+    struct TreeNode *stack1[1000] = {NULL};
+    struct TreeNode *stack2[1000] = {NULL};
+    
+    if (NULL == root)
+        return true;        // empty tree is symmetric
+       
+    ptr1 = root->left;
+    ptr2 = root->right;     /* if push root into stack, will cost 2 times */
+    
+    while(s1 > 0 || s2 > 0 || ptr1 != NULL || ptr2 != NULL)
+    {
+        /* push left node untill the depthest */
+        while(ptr1)
+        {
+            stack1[s1++] = ptr1;
+            ptr1 = ptr1->left;
+        }
+        /* push right node untill the depthest */
+        while(ptr2)
+        {
+            stack2[s2++] = ptr2;
+            ptr2 = ptr2->right;
+        }
+        
+        if (s1 != s2)       /* not the same depth  */
+        {
+            return false;
+        }
+        
+        /* pop the node to compare */
+        ptr1 = (stack1[--s1]);
+        ptr2 = (stack2[--s2]);
+
+        if (ptr1->val != ptr2->val)
+            return false;
+        /* search from anther branch */
+        ptr1 = ptr1->right;
+        ptr2 = ptr2->left;
+    }
+    return true;
+
+}
+
