75aca0320b11e1d3de7321c489e9ffb36b6fca88
anderzheng <zhengqihua@163.com>
2019-09-22 23:38:48 +0800

    Update ex101.c

--- C/ex101.c
+++ C/ex101.c
@@ -89,4 +89,68 @@ bool isSymmetric(struct TreeNode* root){
     return true;
 
 }
+//=============================================================================================
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+#define  QUE_SIZE       1000
+
+/* iteratively method of BFS */
+
+bool isSymmetric(struct TreeNode* root){
+    
+    int head1 = 0, tail1 = 0, head2  = 0, tail2 = 0;
+    struct TreeNode *ptr1 = NULL, *ptr2 = NULL;
+    struct TreeNode *queue1[QUE_SIZE] = {NULL};
+    struct TreeNode *queue2[QUE_SIZE] = {NULL};
+    
+    if (NULL == root)
+        return true;        // empty tree is symmetric
+       
+    queue1[0] = root->left;
+    queue2[0] = root->right;     /* if push root into stack, will cost 2 times */
+    tail1 = tail2 = 1;
+    
+    while(tail1 != head1 || tail2 != head2)
+    {
+        if (tail1-head1 != tail2-head2) /* notice: the length may be diffrent between  queue1 and queue2 */
+            return false;               /* without this check, line 41 will run error */
+        
+        /* get the first node from queue  */
+        ptr1 = queue1[head1];
+        head1 = (head1+1)%QUE_SIZE;
+        
+        ptr2 = queue2[head2];
+        head2 = (head2+1)%QUE_SIZE;
+
+        if (NULL == ptr1 && NULL == ptr2)
+            continue;
+
+        if (NULL == ptr1 || NULL == ptr2)
+            return false;
+
+        if (ptr1->val != ptr2->val) 
+            return false;
+        
+        /* queue1 save from left to right */
+        queue1[tail1] = ptr1->left;
+        tail1 = (tail1+1)% QUE_SIZE;
+        queue1[tail1] = ptr1->right;
+        tail1 = (tail1+1)% QUE_SIZE;
+        
+        /* queue2 save from right to left */
+        queue2[tail2] = ptr2->right;
+        tail2 = (tail2+1)% QUE_SIZE;
+        queue2[tail2] = ptr2->left;
+        tail2 = (tail2+1)% QUE_SIZE;
+    }
+    return true;
+
+}
 
