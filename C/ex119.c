cb487e90c36bf0d2f936462ccd2a982fab456df3
anderzheng <zhengqihua@163.com>
2019-10-02 01:21:49 +0800

    Update ex119.c

--- C/ex119.c
+++ C/ex119.c
@@ -45,3 +45,47 @@ int* getRow(int rowIndex, int* returnSize){
     return dp;
 }
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* using dynamic programing, a normal way with two temp variable */
+/* passed: 4ms */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getRow(int rowIndex, int* returnSize){
+    int * dp = NULL;
+    int i=0, j=0;
+    int prev = 0, current = 0;
+    
+    if (rowIndex < 0)
+    {
+        *returnSize = 0;
+        return NULL;
+    }
+    
+    *returnSize = rowIndex+1;
+    dp = (int*)malloc(*returnSize*sizeof(int));
+    if (NULL == dp)
+    {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    for(i=0;i<rowIndex+1; i++)
+    {
+        dp[0] = 1;
+        dp[i] = 1;
+        prev = 1;
+        for(j=1; j<=i/2; j++)
+        {
+            current = dp[j];        // notice: using temp var to save the dp[j];
+            dp[j] = prev+dp[j];
+            dp[i-j] = dp[j];
+            prev = current;
+        }
+    }
+    
+    return dp;
+}
+
