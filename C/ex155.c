203448bb35f1102c2971e1f92321d03b5cae71fc
anderzheng <zhengqihua@163.com>
2019-10-04 00:59:00 +0800

    Update ex155.c

--- C/ex155.c
+++ C/ex155.c
@@ -1,3 +1,100 @@
+/* using 2 stacks, passed: 32ms */
+
+#define     MAX_SIZE       1024
+typedef struct {
+    int  buff[MAX_SIZE];
+    int  min[MAX_SIZE];
+    //int head;
+    int tail;
+    //int minVal;
+    //int minIdx;
+    
+} MinStack;
+
+/** initialize your data structure here. */
+
+MinStack* minStackCreate() {
+    MinStack *ptr = NULL;
+    
+    ptr = (MinStack*)malloc(sizeof(MinStack));
+    if (NULL == ptr)
+    {
+        return NULL;
+    }
+    memset(ptr, 0, sizeof(MinStack));
+    return ptr;
+    
+}
+
+void minStackPush(MinStack* obj, int x) {
+    if(NULL == obj)
+        return;
+    
+    if (obj->tail <MAX_SIZE)
+    {
+        obj->buff[obj->tail] = x;
+        if (obj->tail == 0)
+            obj->min[obj->tail] = x;
+        else if (obj->min[obj->tail-1] > x)
+        {
+            obj->min[obj->tail] = x;
+        }
+        else
+            obj->min[obj->tail] = obj->min[obj->tail -1];   // don't forget this.
+        obj->tail ++;
+    }
+    
+}
+
+void minStackPop(MinStack* obj) {
+    int i=0;
+    if(NULL == obj || obj->tail <=0)
+        return;
+    
+    if (obj->tail > 0)
+    {
+        obj->tail --;
+        obj->buff[obj->tail] = 0;
+        obj->min[obj->tail] = 0;
+    }  
+}
+
+int minStackTop(MinStack* obj) {
+    if(NULL == obj || obj->tail<=0)
+        return -1;
+    
+    return obj->buff[obj->tail-1];
+}
+
+int minStackGetMin(MinStack* obj) {
+    if(NULL == obj || obj->tail <=0)
+        return -1;
+  
+    return obj->min[obj->tail -1];
+}
+
+void minStackFree(MinStack* obj) {
+    if(NULL == obj)
+        return;
+    
+    free(obj);
+}
+
+/**
+ * Your MinStack struct will be instantiated and called as such:
+ * MinStack* obj = minStackCreate();
+ * minStackPush(obj, x);
+ 
+ * minStackPop(obj);
+ 
+ * int param_3 = minStackTop(obj);
+ 
+ * int param_4 = minStackGetMin(obj);
+ 
+ * minStackFree(obj);
+*/
+//--------------------------------------------------------------------------
+
 /* passed: 36ms */
 
 #define     MAX_SIZE       1024
