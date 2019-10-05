2a9f3994929ca89529dc916143644d9912c9fbca
anderzheng <zhengqihua@163.com>
2019-10-05 15:29:29 +0800

    Create ex167.c

---
+++ ex167.c
@@ -0,0 +1,49 @@
+/* using normal method: passed 12ms */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
+    int diff = 0;
+    int i = 0;
+    int left = 0, right = 0, mid = 0;
+    int *res = NULL;
+    
+    for(; i<numbersSize; i++)
+    {
+        diff = target - numbers[i];
+        if (diff < numbers[i] || diff > numbers[numbersSize-1])
+            continue;
+        left = i+1;
+        right = numbersSize;
+        while(left <= right)
+        {
+            mid = (left+right)/2;
+            if (diff > numbers[mid])
+            {
+                left = mid+1;
+            }
+            else if(diff < numbers[mid])
+            {
+                right = mid-1;
+            }
+            else
+            {
+                res = (int*)malloc(sizeof(int)*2);
+                if (NULL == res)
+                {
+                    *returnSize = 0;
+                    return NULL;
+                }
+                res[0] = i+1;
+                res[1] = mid+1;
+                *returnSize = 2;
+                return res;
+            }
+        }
+    }
+    
+    *returnSize = 0;
+    return NULL;
+}
+
