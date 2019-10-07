8eaaece09ea12ff98a166be07d891ce8327286f8
anderzheng <zhengqihua@163.com>
2019-10-08 01:04:51 +0800

    Update ex191.c

--- C/ex191.c
+++ C/ex191.c
@@ -1,3 +1,15 @@
+/* new method: using n&n-1 to delete the last 1 in right */
+
+int hammingWeight(uint32_t n) {
+    int res = 0;
+    while(n!=0)
+    {
+        res++;
+        n = n&(n-1);
+    }
+    return res;
+}
+//---------------------------------------------------------------------
 /* passed: 0ms */
 
 int hammingWeight(uint32_t n) {
