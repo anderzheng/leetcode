d958c588b37da85e7f40278e628bf82ae359081e
anderzheng <zhengqihua@163.com>
2019-10-08 00:54:19 +0800

    Update ex190.c

--- C/ex190.c
+++ C/ex190.c
@@ -1,3 +1,20 @@
+/* normal method , passed 0ms */
+
+uint32_t reverseBits(uint32_t n) {
+    uint32_t res = 0;
+    int i = 0;
+    
+    for(;i<32; i++)
+    {
+        res <<= 1;
+        res += (n&1);
+        n>>=1;
+    }
+    
+    return res;
+}
+
+//-----------------------------------------------
 /* normal method, passed: 4ms */
 
 uint32_t reverseBits(uint32_t n) {
