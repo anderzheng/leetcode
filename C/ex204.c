789e20ace746f6cbf648e6d515bbd955507d7c21
anderzheng <zhengqihua@163.com>
2019-10-10 00:57:53 +0800

    Update ex204.c

--- C/ex204.c
+++ C/ex204.c
@@ -1,3 +1,35 @@
+/* using  Sieve of Eratosthenes method */
+/* passed 52ms */
+
+int countPrimes(int n)
+{
+    int *buff = NULL;
+    int i = 0, j = 0, cnt = 0;
+    
+    if (n <= 2)
+        return 0;
+    
+    buff = (int*)malloc(sizeof(int)*n);
+    if (NULL == buff)
+        return 0;
+    for(;i<n;i++)
+        buff[i] = 1;
+    
+    for(i=2;i*i<=n; i++)
+    {
+        for(j=i*i;j<n;j+=i)  // notice
+        {
+            buff[j] = 0;  // j = i*times;
+        }
+    }
+    for(i=2; i<n; i++)      // skip 0 and 1
+        cnt += buff[i];
+    
+    free(buff);
+    return cnt;
+}
+
+#if 0
 /* normal method, passed 692 ms */
 
 bool isPrime(int n)
@@ -26,3 +58,5 @@ int countPrimes(int n){
     
     return cnt;
 }
+
+#endif
